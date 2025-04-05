b94cf6c5d9ace8d26e30a37244b2dc47ac108a8a
秦诗染 <1145252643@qq.com>
2025-04-06 03:24:12 +0800

    麻了怎么这么难写

--- src/captcha.rs
+++ src/captcha.rs
@@ -1,7 +1,69 @@
-use crate::model::Yolo11nModel;
+use std::error::Error;
+use std::sync::Arc;
+use ort::session::Session;
+use crate::config::{DefaultLoader, ModelLoader};
+use crate::model::{ModelEnum};
+
+enum CaptchaEnum {
+    ChineseClick0,
+}
+
+struct CaptchaEngine {
+    captcha_breaker: Box<dyn CaptchaBreaker>,
+}
+
+impl CaptchaEngine {
+    fn build() -> CaptchaEngineBuilder {
+        CaptchaEngineBuilder::new()
+    }
+}
+
+struct CaptchaEngineBuilder {
+    model_loader: Option<Arc<dyn ModelLoader>>,
+    captcha_breaker: Option<CaptchaEnum>,
+}
+impl CaptchaEngineBuilder {
+    fn new() -> Self {
+        CaptchaEngineBuilder{
+            model_loader: None,
+            captcha_breaker: None,
+        }
+    }
+    fn with_model_loader(mut self, model_loader: Arc<dyn ModelLoader>) -> Self {
+        self.model_loader = Some(model_loader);
+        self
+    }
+    fn with_captcha_breaker(mut self, breaker: CaptchaEnum) -> Self {
+        self.captcha_breaker = Some(breaker);
+        self
+    }
+    fn build(self) -> Result<CaptchaEngine, Box<dyn Error>> {
+        let model_loader = self.model_loader.unwrap_or_else(|| Arc::from(DefaultLoader::default()));
+        // todo!(错误处理)
+        let captcha_breaker = Box::from(match self.captcha_breaker.unwrap() {
+            CaptchaEnum::ChineseClick0 => {
+                ChineseClick0 {
+                    yolo11n_model: model_loader.load(ModelEnum::Yolo11n)?,
+                    siamese_model: model_loader.load(ModelEnum::Siamese)?,
+                }
+            }
+        });
+        Ok(CaptchaEngine {
+            captcha_breaker
+        })
+    }
+}
+
+trait CaptchaBreaker {
+    fn run(&self, inputs: dyn Inputs) -> Result<dyn Outputs, Box<dyn Error>>;
+}
+
+trait Outputs {}
+trait Inputs {}
 
 #[cfg(feature = "chinese_click_0")]
 struct ChineseClick0 {
-    yolo11_model: Yolo11nModel,
+    yolo11n_model: Session,
+    siamese_model: Session,
 }
-
+impl CaptchaBreaker for ChineseClick0 {}
--- src/config.rs
+++ src/config.rs
@@ -1,68 +1,50 @@
 use std::env;
-use std::path::PathBuf;
-use std::sync::Mutex;
-use once_cell::sync::Lazy;
+use std::error::Error;
+use std::fmt::Debug;
+use std::fs::File;
+use std::io::Write;
+use std::path::{Path};
+use ort::session::Session;
+use crate::model::{ModelEnum};
 
-#[derive(Debug)]
-pub struct GlobalConfig {
-    root_path: PathBuf,
-    pub models_config: ModelsConfig,
-}
 
-impl Default for GlobalConfig {
-    fn default() -> Self {
-        let root_path = env::current_dir().unwrap();
-        let models_config = ModelsConfig::new(root_path.join("models"));
-        GlobalConfig {
-            root_path,
-            models_config,
+#[derive(Default,Debug)]
+pub struct DefaultLoader;
+impl ModelLoader for DefaultLoader {}
+
+pub trait ModelLoader: Sync + Send + Debug {
+    fn load(&self, model_enum: ModelEnum) -> Result<Session, Box<dyn Error>> {
+
+        let mut model_path =  env::current_dir()?.join("captcha_models");
+        let mut url = "";
+        match model_enum {
+            ModelEnum::Yolo11n => {
+                model_path = model_path.join("yolov11n_captcha.onnx");
+                url = "https://www.modelscope.cn/models/Amorter/CaptchaBreakerModels/resolve/master/yolov11n_captcha.onnx";
+            }
+            ModelEnum::Siamese => {
+                model_path = model_path.join("siamese.onnx");
+                url = "https://www.modelscope.cn/models/Amorter/CaptchaBreakerModels/resolve/master/siamese.onnx";
+            }
         }
+        self.build(model_path.to_str().unwrap(), url)
     }
-}
-
-impl GlobalConfig {
-    pub fn instance() -> &'static GlobalConfig {
-        static INSTANCE: Lazy<GlobalConfig> = Lazy::new(|| GlobalConfig::default());
-        &INSTANCE
-    }
-}
-
-/// 模型配置
-/// # Fields
-/// - `file_path`: 模型文件的保存路径
-/// - `download_url`: 模型文件的下载链接
-#[derive(Debug)]
-pub struct ModelConfig {
-    pub file_path: String,
-    pub download_url: String,
-}
-
-impl ModelConfig {
-    fn new(file_path: &str, download_url: &str) -> ModelConfig {
-        ModelConfig {
-            file_path: file_path.to_string(),
-            download_url: download_url.to_string()
+    fn build(&self, save_path: &str, download_url: &str) -> Result<Session, Box<dyn Error>> {
+        let model_path = Path::new(save_path);
+        if model_path.exists() {
+            let session = Session::builder()?.commit_from_file(model_path)?;
+            return Ok( session );
         }
+        let model_bytes = reqwest::blocking::get(download_url)?.bytes()?;
+        // 保存文件
+        File::create(model_path)?.write_all(model_bytes.as_ref())?;
+        let model =  self.build_from_memory(model_bytes.as_ref());
+        Ok( model? )
+
     }
-}
-
-#[derive(Debug)]
-pub struct ModelsConfig {
-    pub yolo11n: ModelConfig,
-    pub siamese: ModelConfig,
-}
-
-impl ModelsConfig {
-    fn new(models_dir: PathBuf) -> Self {
-        ModelsConfig {
-            yolo11n: ModelConfig::new(
-                models_dir.join("yolov11n_captcha.onnx").to_str().unwrap(),
-                "https://www.modelscope.cn/models/Amorter/CaptchaBreakerModels/resolve/master/yolov11n_captcha.onnx",
-            ),
-            siamese: ModelConfig::new(
-                models_dir.join("siamese.onnx").to_str().unwrap(),
-                "https://www.modelscope.cn/models/Amorter/CaptchaBreakerModels/resolve/master/siamese.onnx",
-            ),
-        }
+    /// 直接从内存中构建模型
+    fn build_from_memory(&self, model_bytes: &[u8]) -> Result<Session, Box<dyn Error>> {
+        let session = Session::builder()?.commit_from_memory(model_bytes)?;
+        Ok( session )
     }
 }
